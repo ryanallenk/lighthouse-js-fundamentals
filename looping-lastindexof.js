35ca8151b279657b0d42a6469b6dae3aee569b82
ryanallenk <ryanallenk@gmail.com>
2022-02-18 17:08:26 -0500

    Completed second looping exercise

---
+++ looping-lastindexof.js
@@ -0,0 +1,13 @@
+function lastIndexOf(array, value) {
+//  let result = 0;
+  for (let i = array.length; i >= 0; i--) {
+    if (array[i] === value)
+      return i;
+  } 
+  return -1;
+}
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
