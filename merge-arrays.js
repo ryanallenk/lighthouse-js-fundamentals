76a1fc588ac34160a4e0c0071367a949e703a232
ryanallenk <ryanallenk@gmail.com>
2022-03-02 11:01:49 -0500

    Completed stretch activity merging and sorting arrays

---
+++ merge-arrays.js
@@ -0,0 +1,11 @@
+function merge(firstArr, secondArr) {
+  let newArr = firstArr 
+  for (let i = 0; i < secondArr.length; i++) {
+    firstArr.push(secondArr[i]);
+  }
+  return newArr.sort();
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
